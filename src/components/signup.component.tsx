828c0a3d37159376c2c034c5a5dd7e264178396c
Divyanshu Motivaras <divyanshu1593@gmail.com>
2024-02-12 19:05:01 +0530

    redirecting back to welcome screen

--- src/components/signup.component.tsx
+++ src/components/signup.component.tsx
@@ -1,33 +1,38 @@
+import { useNavigate } from "react-router-dom";
+
 export function Signup() {
+  const navigate = useNavigate();
+
+  const clickHandler = async (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+    event.preventDefault();
+    const username = (document.getElementById('username') as HTMLInputElement).value;
+    const password = (document.getElementById('password') as HTMLInputElement).value;
+
+    const res = await (await fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({username, password}),
+    })).json();
+    
+    if (res.isError) {
+      alert('username already exists');
+      if (res.message.message === 'username already exist') {
+        return ;
+      }
+      alert('signup failed');
+      return ;
+    }
+    alert('signup done');
+    navigate('/');
+  }
   return (
     <>
       <form>
         username: <input type="text" id="username" required/>
         password: <input type="password" id="password" required/>
-        <input type="submit" onClick={async (event) => {
-          event.preventDefault();
-          const username = (document.getElementById('username') as HTMLInputElement).value;
-          const password = (document.getElementById('password') as HTMLInputElement).value;
-
-          const res = await (await fetch('http://localhost:3000/signup', {
-            method: 'POST',
-            headers: {
-              "Content-Type": "application/json",
-              // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: JSON.stringify({username, password}),
-          })).json();
-          
-          if (res.isError) {
-            if (res.message.message === 'username already exist'){
-              alert('username already exists');
-              return ;
-            }
-            alert('signup failed');
-            return ;
-          }
-          alert('signup done');
-        }}/>
+        <input type="submit" onClick={(event) => clickHandler(event)}/>
       </form>
     </>
   );
