5302a4ac1df964b6376ba76ed70ba11a6ed6dbe4
Divyanshu Motivaras <divyanshu1593@gmail.com>
2024-02-15 10:57:08 +0530

    level btn now become green on crossed level

--- src/components/level-btn.component.tsx
+++ src/components/level-btn.component.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { useAppSelector } from "../hooks/redux-hooks";
 import { levelType } from "../levels";
 
 export const LevelBtn = ({ levelNumber, lvl, clickHandler }: {
@@ -5,10 +7,48 @@ export const LevelBtn = ({ levelNumber, lvl, clickHandler }: {
   lvl: levelType,
   clickHandler: (lvl: levelType, lvlNum: number) => void
 }) => {
+  const [isCrossed, setIsCrossed] = useState(false);
+  const jwtToken = useAppSelector(state => state.user.value);
+
+  const updateIfCrossed = async () => {
+    const res = await (await fetch(`http://localhost:3000/is-level-crossed/${levelNumber}`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    })).json();
+
+    console.log(res);
+
+    if (!res.isError) {
+      if (res.data) {
+        setIsCrossed(true);
+      }
+    }
+  }
+
+  useEffect(() => {
+    updateIfCrossed();
+  }, []);
+
+  if (isCrossed) {
+    return (
+      <button id='level-btns'
+      style={{
+        backgroundColor: 'green',
+      }}
+      onClick={() => {
+        return clickHandler(lvl, levelNumber);
+      }}
+      >{ levelNumber }</button>
+    );
+  }
+
   return (
     <button id='level-btns'
     onClick={() => {
-      return clickHandler(lvl, levelNumber)
+      return clickHandler(lvl, levelNumber);
     }}
     >{ levelNumber }</button>
   );
