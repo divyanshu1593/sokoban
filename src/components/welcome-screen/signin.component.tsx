1977acd53a58e557849a564bc8d0dc603eafed10
Divyanshu Motivaras <divyanshu1593@gmail.com>
2024-02-20 16:24:53 +0530

    restricting signin and signup req while the old req is been processed

--- src/components/welcome-screen/signin.component.tsx
+++ src/components/welcome-screen/signin.component.tsx
@@ -1,15 +1,17 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { signin } from "../../state-slices/user.slice";
 import { useAppDispatch } from "../../hooks/redux-hooks";
+import LoadingGif from '../../images/loading.svg';
 
 export const Signin = () => {
   const usernameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  const clickHandler = async (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+  const clickHandler = (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
     event.preventDefault();
     const username = usernameRef.current!.value;
     const password = passwordRef.current!.value;
@@ -24,39 +26,46 @@ export const Signin = () => {
       return ;
     }
 
-    try {
-      const res = await (await fetch(`${process.env.REACT_APP_BACKEND_URL}/auth/signin`, {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, password }),
-      })).json();
-
-      if (res.isError) {
-        if (res.message.status === 401) {
-          alert('incorrect username or password!');
-          return ;
+    const handleSignIn = async () => {
+      try {
+        const res = await (await fetch(`${process.env.REACT_APP_BACKEND_URL}/auth/signin`, {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        })).json();
+        setIsLoading(false);
+  
+        if (res.isError) {
+          if (res.message.status === 401) {
+            alert('incorrect username or password!');
+            return ;
+          }
+          throw new Error(res.message);
         }
-        throw new Error(res.message);
+  
+        alert('signin done');
+        dispatch(signin(res.data));
+        navigate('/');
+      } catch (err) {
+        console.log(err);
+        alert('signin not done');
       }
+    }
 
-      alert('signin done');
-      dispatch(signin(res.data));
-      navigate('/');
-    } catch (err) {
-      console.log(err);
-      alert('signin not done');
-    } 
-      
+    setIsLoading(true);
+    handleSignIn();
   }
+
   return (
     <div className="auth-box">
       <div className="login-box-items">SignIn</div>
       <form>
         <div className="login-box-items">username <input className="login-box-input"  ref={usernameRef} type="text" /></div>
         <div className="login-box-items">password <input className="login-box-input" ref={passwordRef} type="password" /></div>
-        <div className="login-box-items"><input className="submit-btn" type="submit" onClick={clickHandler}/></div>
+        {!isLoading && <div className="login-box-items"><input className="submit-btn" type="submit" onClick={clickHandler}/></div>}
+        {isLoading && <div className="login-box-items"><input className="submit-btn" type="image" src={LoadingGif} alt="loading..."/></div>}
       </form>
     </div>
   );
--- src/components/welcome-screen/signup.component.tsx
+++ src/components/welcome-screen/signup.component.tsx
@@ -1,12 +1,14 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import LoadingGif from '../../images/loading.svg';
 
 export const Signup = () => {
   const navigate = useNavigate();
   const usernameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const clickHandler = async (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+  const clickHandler = (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
     event.preventDefault();
     const username = usernameRef.current!.value;
     const password = passwordRef.current!.value;
@@ -21,24 +23,31 @@ export const Signup = () => {
       return ;
     }
 
-    const res = await (await fetch(`${process.env.REACT_APP_BACKEND_URL}/auth/signup`, {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({username, password}),
-    })).json();
-    
-    if (res.isError) {
-      if (res.message.message === 'username already exist') {
-        alert('username already exists');
+    const handleSignUP = async () => {
+      const res = await (await fetch(`${process.env.REACT_APP_BACKEND_URL}/auth/signup`, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({username, password}),
+      })).json();
+
+      setIsLoading(false);
+      
+      if (res.isError) {
+        if (res.message.message === 'username already exist') {
+          alert('username already exists');
+          return ;
+        }
+        alert('signup failed');
         return ;
       }
-      alert('signup failed');
-      return ;
+      alert('signup done');
+      navigate('/');
     }
-    alert('signup done');
-    navigate('/');
+
+    setIsLoading(true);
+    handleSignUP();
   }
   return (
     <div className="auth-box">
@@ -46,7 +55,8 @@ export const Signup = () => {
       <form>
         <div className="login-box-items">username: <input className="login-box-input" ref={usernameRef} type="text" id="username" required/></div>
         <div className="login-box-items">password: <input className="login-box-input" ref={passwordRef} type="password" id="password" required/></div>
-        <div className="login-box-items"><input className="submit-btn" type="submit" onClick={(event) => clickHandler(event)}/></div>
+        {!isLoading && <div className="login-box-items"><input className="submit-btn" type="submit" onClick={(event) => clickHandler(event)}/></div>}
+        {isLoading && <div className="login-box-items"><input className="submit-btn" type="image" src={LoadingGif} alt="loading..."/></div>}
       </form>
     </div>
   );
